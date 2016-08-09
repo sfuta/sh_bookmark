83341dc2fa44fb450ade4e9d2be4de9d59f3dd74
shimpei_futagami <shimpei.futagami@4digit.jp>
2016-08-09 15:42:15 +0900

    [sh script] bookmark処理更新:help等のメッセージ表示処理追加

--- tool/sh/dev/sh_bookmark/sb_main.sh
+++ tool/sh/dev/sh_bookmark/sb_main.sh
@@ -5,12 +5,21 @@ source `dirname $0`/sb_command.sh
 
 sh_bookmark ()
 {
-  [ -z $1 ] && __sh_bookmark::selected "a";
-
   case "-$1" in
+    "-")   __sh_bookmark::selected "a";;
     "--a") __sh_bookmark::add $2 $3;;
     "--d") __sh_bookmark::delete;;
     "--r") __sh_bookmark::reload;;
-    "*")   echo "message記載予定";;
+    "--h")
+      echo "bookmark command for zsh"
+      echo "  "$0"                show registed bookmark list"
+      echo "  "$0" -a [path] [id] add bookmark"
+      echo "  "$0" -d             delete bookmark(use peco)"
+      echo "  "$0" -r             reload bookmark"
+    ;;
+    *)
+      echo "illegal option "$1
+      echo "please check option:use \""$0" -h\" show help"
+    ;;
   esac
 }
